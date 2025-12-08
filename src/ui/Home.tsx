e63c1c251396963373cac2a76bde42d62b195ae8
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-12-08 20:03:59 +0100

    add tailwind

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <header>
-            <h1>
+            <h1 className="text-center text-xl font-semibold text-yellow-500">
                 The best pizza.
                 <br />
                 Straight out of the oven, straight to you.
